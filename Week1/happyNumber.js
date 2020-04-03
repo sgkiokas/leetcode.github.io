58bd9e828edde199206168f4be2a043d39a543bc
sgkiokas <s_giokas@hotmail.com>
2020-04-03 15:14:08 +0100

    chore: add exercises; descriptions (#2)
    The exercise descriptions have been added.
    
    Signed-off-by: Stelios Gkiokas <s_giokas@hotmail.com>

--- Week1/happyNumber.js
+++ Week1/happyNumber.js
@@ -1,3 +1,13 @@
+/*
+ * Exercise description: 
+ *
+ * Write an algorithm to determine if a number is "happy".
+ *
+ * A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares
+ * of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those
+ * numbers for which this process ends in 1 are happy numbers.
+ */
+
 let isHappy = n => {
     if(n === 1) {
         return true;

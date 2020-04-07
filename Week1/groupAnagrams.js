b8f61b0bb9f2a0d92c4cb8fc38d483bb8552724c
Stelios Gkiokas <Stylianos.Gkiokas@ibm.com>
2020-04-07 19:50:35 +0100

    feat: grouped anagrams exercise
    This is the solution of the fifth exercise of the April 2020 leetcode
    challenge.
    
    Signed-off-by: Stelios Gkiokas <s_giokas@hotmail.com>

---
+++ Week1/groupAnagrams.js
@@ -0,0 +1,32 @@
+/*
+ * Exercise description: 
+ *
+ * Given an array of strings, group anagrams together.
+ * 
+ * Note:
+ * - All inputs will be in lowercase.
+ * - The order of your output does not matter.
+ * 
+ */
+
+let groupAnagrams = strs => {
+    let anagramsMapper = {};
+    let result = [];
+
+    strs.forEach(element => {
+        // use the sorted letters as the common lookup key for each of the entries
+        let letters = element.split('').sort();
+
+        if(anagramsMapper[letters]) {
+            anagramsMapper[letters].push(element)
+        } else {
+            anagramsMapper[letters] = [element];
+        }
+    });
+
+    Object.entries(anagramsMapper).forEach(element => {
+        result.push(element[1]);
+    });
+
+    return result;
+}

01082102717ebaf82ed08b29dcf95ce6793f9d2b
Stelios Gkiokas <Stylianos.Gkiokas@ibm.com>
2020-04-25 23:27:29 +0100

    feat: Search rotated sorted array exercise
    As per title.
    
    Signed-off-by: Stelios Gkiokas <s_giokas@hotmail.com>

---
+++ Week3/searchRotatedSortedArray.js
@@ -0,0 +1,22 @@
+/*
+ * Exercise description: 
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * 
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ */
+
+ /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let search = (nums, target) => {
+    return nums.indexOf(target);
+};
